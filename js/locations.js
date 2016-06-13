451527e83a4d767b160fe29022d9335323ae145e
Alexander C Truong <actruong3@wichita.edu>
2016-06-13 15:33:06 -0500

    Remove ID selector from main.css file. Move all selectors to its own line in main.css.

--- js/locations.js
+++ js/locations.js
@@ -1,65 +1,63 @@
 ﻿
-(function () {
-    restaurantLocations = [
-        {
-            name: "Pie Five Pizza Co. East",
-            location: { lat: 37.720087, lng: -97.245745 }
-        },
-        {
-            name: "Pie Five Pizza Co. West",
-            location: { lat: 37.678793, lng: -97.425300 }
-        },
-        {
-            name: "The Anchor",
-            location: { lat: 37.685747, lng: -97.324861 }
-        },
-        {
-            name: "Bricktown Brewery West",
-            location: { lat: 37.722525, lng: -97.443843 }
-        },
-        {
-            name: "Bricktown Brewery East",
-            location: { lat: 37.720376, lng: -97.245498 }
-        },
-        {
-            name: "Twelve Restaurant & Bar",
-            location: { lat: 37.678520, lng: -97.481292 }
-        },
-        {
-            name: "Tanya's Soup Kitchen",
-            location: { lat: 37.686001, lng: -97.316451 }
-        },
-        {
-            name: "Taste of Philly",
-            location: { lat: 37.722109, lng: -97.309690 }
-        },
-        {
-            name: "Dempsey's Burger Pub",
-            location: { lat: 37.687129, lng: -97.293839 }
-        },
-        {
-            name: "Pho Hot",
-            location: { lat: 37.690902, lng: -97.243765 }
-        },
-        {
-            name: "Texas Roadhouse",
-            location: { lat: 37.672288, lng: -97.419899 }
-        },
-        {
-            name: "Margherita's Cantina",
-            location: { lat: 37.686435, lng: -97.299906 }
-        },
-        {
-            name: "Wasabi Sushi Bar",
-            location: { lat: 37.741847, lng: -97.243160 }
-        },
-        {
-            name: "Wasabi Hinkaku",
-            location: { lat: 37.689357, lng: -97.327670 }
-        },
-        {
-            name: "Wichita Brewing Company",
-            location: { lat: 37.708687, lng: -97.445128 }
-        }
-    ];
-})();
+var restaurantLocations = [
+    {
+        name: "Pie Five Pizza Co. East",
+        location: { lat: 37.720087, lng: -97.245745 }
+    },
+    {
+        name: "Pie Five Pizza Co. West",
+        location: { lat: 37.678793, lng: -97.425300 }
+    },
+    {
+        name: "The Anchor",
+        location: { lat: 37.685747, lng: -97.324861 }
+    },
+    {
+        name: "Bricktown Brewery West",
+        location: { lat: 37.722525, lng: -97.443843 }
+    },
+    {
+        name: "Bricktown Brewery East",
+        location: { lat: 37.720376, lng: -97.245498 }
+    },
+    {
+        name: "Twelve Restaurant & Bar",
+        location: { lat: 37.678520, lng: -97.481292 }
+    },
+    {
+        name: "Tanya's Soup Kitchen",
+        location: { lat: 37.686001, lng: -97.316451 }
+    },
+    {
+        name: "Taste of Philly",
+        location: { lat: 37.722109, lng: -97.309690 }
+    },
+    {
+        name: "Dempsey's Burger Pub",
+        location: { lat: 37.687129, lng: -97.293839 }
+    },
+    {
+        name: "Pho Hot",
+        location: { lat: 37.690902, lng: -97.243765 }
+    },
+    {
+        name: "Texas Roadhouse",
+        location: { lat: 37.672288, lng: -97.419899 }
+    },
+    {
+        name: "Margherita's Cantina",
+        location: { lat: 37.686435, lng: -97.299906 }
+    },
+    {
+        name: "Wasabi Sushi Bar",
+        location: { lat: 37.741847, lng: -97.243160 }
+    },
+    {
+        name: "Wasabi Hinkaku",
+        location: { lat: 37.689357, lng: -97.327670 }
+    },
+    {
+        name: "Wichita Brewing Company",
+        location: { lat: 37.708687, lng: -97.445128 }
+    }
+];
